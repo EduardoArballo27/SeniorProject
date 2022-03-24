a8d0393834ba61992cf3cc4911e1b3ee7d218436
EduardoArballo27 <51279560+EduardoArballo27@users.noreply.github.com>
2022-03-24 10:15:05 -0700

    Update Digispark_Student_Sample.ino

--- Digispark_Student_Sample.ino
+++ Digispark_Student_Sample.ino
@@ -19,10 +19,13 @@ void loop() {
   DigiKeyboard.delay(600);
   //Types in a simple message
   DigiKeyboard.print("Hi!");
+  DigiKeyboard.delay(600);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("I am the DigiSpark Board!");
+  DigiKeyboard.delay(600);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("I am capable of taking control of your computer and doing what I am told to do");
+  DigiKeyboard.delay(600);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("I can do much more than type simple messages, such as doing this...");
   DigiKeyboard.delay(600);
