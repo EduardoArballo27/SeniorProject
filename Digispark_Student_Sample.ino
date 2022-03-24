e3f0b9073865bd90d489d8b613baa9944a8890b1
EduardoArballo27 <51279560+EduardoArballo27@users.noreply.github.com>
2022-03-24 10:03:50 -0700

    Update and rename Digispark_Student_Sample to Digispark_Student_Sample.ino

---
+++ Digispark_Student_Sample.ino
@@ -0,0 +1,44 @@
+#include "DigiKeyboard.h"
+#define KEY_TAB      0x2b
+
+void setup() {
+  pinMode(1, OUTPUT);
+}
+
+void loop() {
+  //Small delay to make sure DigiSpark is fully connected
+  digitalWrite(1, HIGH);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(0);
+  //Opens up Notepad
+  DigiKeyboard.sendKeyStroke('r',MOD_GUI_LEFT);
+  DigiKeyboard.delay(800);
+  DigiKeyboard.print("Notepad");
+  DigiKeyboard.delay(600);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(600);
+  //Types in a simple message
+  DigiKeyboard.print("Hi!");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("I am the DigiSpark Board!");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("I am capable of taking control of your computer and doing what I am told to do");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("I can do much more than type simple messages, such as doing this...");
+  DigiKeyboard.delay(600);
+  //Flashes LED when done
+  digitalWrite(1,LOW);
+  DigiKeyboard.delay(500);
+  digitalWrite(1,HIGH);
+  DigiKeyboard.delay(500);
+  digitalWrite(1,LOW);
+  DigiKeyboard.delay(500);
+  digitalWrite(1,HIGH);
+  DigiKeyboard.delay(500);
+  digitalWrite(1,LOW);
+  DigiKeyboard.delay(500);
+  digitalWrite(1,HIGH);
+  DigiKeyboard.delay(500);
+  digitalWrite(1,LOW);
+  for(;;){ /*empty*/ }
+}
